02a5a1d890a149bf511641665b2f905a365f6c7b
David Smith <dasmith91@gmail.com>
2012-09-25 11:14:16 +0100

    Set up initial Project

---
+++ Client/Assets/src/ABSTRACTCLASSES/ABSTRACT_PlayerEntity.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class ABSTRACT_PlayerEntity : MonoBehaviour {
+	
+	int health = 100;
+	int armour = 0;
+	
+	
+	void __init__()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
