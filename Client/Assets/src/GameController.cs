1211410049d24da6f3a159532320d9262fb3d044
David Smith <dasmith91@gmail.com>
2012-09-27 02:41:06 +0100

    Small .gitignore test, looks like it's working.

--- Client/Assets/src/GameController.cs
+++ Client/Assets/src/GameController.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	float timeScale = 1f;
 	
+	float getTimeScale()
+	{
+		return timeScale;	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void setTimeScale(float timeScale)
+	{
+		this.timeScale = timeScale;
 	}
 }
