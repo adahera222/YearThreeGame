8a0ee156d686a7776c1cc65298c075f8072360f5
David Smith <dasmith91@gmail.com>
2012-10-15 00:47:40 +0100

    Added Getters/Setters to CameraController

--- Client/Assets/src/CameraController.cs
+++ Client/Assets/src/CameraController.cs
@@ -1,28 +1,58 @@
 using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
-
+public class CameraController : MonoBehaviour
+{
 	public Transform target;
-	public float distance, height = 0f; // Will be set in the propertys panel
-	
-	void LateUpdate () {
-	// LateUpdate is triggered after Update(), use for fixing issues where code must be triggered after the main game logic
-		
+	public float distance, height = 0f; // Will be set in the properties panel
+
+	void LateUpdate ()
+	{
+		// LateUpdate is triggered after Update(), use for fixing issues where code must be triggered after the main game logic
+
 		// Intended Pos, will be added to later
 		Vector3 finalVec = target.position;
-		
+
 		// Displace object in reguards to the direction the target is looking
-		finalVec += -target.forward * distance + new Vector3(0, height, 0);
-		
+		finalVec += -target.forward * distance + new Vector3 (0, height, 0);
+
 		// Smooth out motion. Mmmmm buttery smooth.
-		finalVec = Vector3.Lerp(transform.position, finalVec, 7f * Time.deltaTime);
-		
+		finalVec = Vector3.Lerp (transform.position, finalVec, 7f * Time.deltaTime);
+
 		// Apply the transformation
 		transform.position = finalVec;
-		
+
 		// Look at the target
 		transform.LookAt (target);
-		
+	}
+	
+	public Transform getTarget ()
+	{
+		return this.target;
+	}
+
+	public void setTarget (Transform target)
+	{
+		this.target = target;
+	}
+	
+	public float gettarget ()
+	{
+		return this.distance;
+	}
+
+	public void setDistance (float distance)
+	{
+		this.distance = distance;
+	}
+	
+	public float getHeight ()
+	{
+		return this.height;
+	}
+
+	public void setHeight (float height)
+	{
+		this.height = height;
 	}
 }
